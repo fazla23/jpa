398141142896332b2aac21b0545b91401cb66728
fazla23 <fazlarabbi366iou@gmail.com>
2024-09-06 19:02:10 +0600

    Initial commit

---
+++ src/main/java/com/fazla/jpa/models/Author.java
@@ -0,0 +1,21 @@
+package com.fazla.jpa.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+}
