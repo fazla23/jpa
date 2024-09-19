833aeb80029a7de6bc8093eb3badd1e47b507cfd
fz <fazlarabbi366iou@gmail.com>
2024-09-19 23:34:53 +0600

    Specification calling

--- src/main/java/com/fazla/jpa/JpaApplication.java
+++ src/main/java/com/fazla/jpa/JpaApplication.java
@@ -4,11 +4,13 @@ import com.fazla.jpa.models.Author;
 import com.fazla.jpa.models.Video;
 import com.fazla.jpa.repositories.AuthorRepository;
 import com.fazla.jpa.repositories.VideoRepository;
+import com.fazla.jpa.specification.AuthorSpecification;
 import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
@@ -26,6 +28,12 @@ public class JpaApplication {
 		VideoRepository videoRepository
 	){
 		return args -> {
+			Specification<Author> spec = Specification.
+					where(AuthorSpecification.hasAge(32))
+					.and(AuthorSpecification.firstNameLike("%La%"));
+
+			authorRepository.findAll(spec).forEach(System.out::println);
+
 //			Author author = Author.builder()
 //					.id(1)
 //					.firstName("fazla")
@@ -38,11 +46,11 @@ public class JpaApplication {
 //			authorRepository.updateAuthor(32,1);
 //			authorRepository.updateAllAuthorsAges(52);
 			//find by named query
-			List<Author> authors = authorRepository.findByNamedQuery(50);
-			System.out.println(authors);
-
-			//Update with named query
-			authorRepository.updateByNamedQuery(67);
+//			List<Author> authors = authorRepository.findByNamedQuery(50);
+//			System.out.println(authors);
+//
+//			//Update with named query
+//			authorRepository.updateByNamedQuery(67);
 
 
 //			Faker faker = new Faker();
--- src/main/java/com/fazla/jpa/specification/AuthorSpecification.java
+++ src/main/java/com/fazla/jpa/specification/AuthorSpecification.java
@@ -7,7 +7,7 @@ import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 public class AuthorSpecification {
-    public static Specification<Author> authorSpecification(int age) {
+    public static Specification<Author> hasAge(int age) {
         return (Root<Author> root, CriteriaQuery<?> query, CriteriaBuilder builder ) -> {
             if(age < 0)
                 return null;
@@ -21,7 +21,7 @@ public class AuthorSpecification {
                 CriteriaBuilder builder ) -> {
                     if(firstname == null)
                         return null;
-                    return builder.like(root.get("firstname"),firstname);
+                    return builder.like(root.get("firstName"),firstname);
                 };
     }
 }
