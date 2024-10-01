553851013bd9bbfb6e85b91f6f18fa25a5b15a88
fazla23 <fazlarabbi366iou@gmail.com>
2024-10-01 15:06:33 +0600

    Docker compose

--- src/test/java/com/fazla/jpa/JpaApplicationTests.java
+++ src/test/java/com/fazla/jpa/JpaApplicationTests.java
@@ -3,10 +3,10 @@ package com.fazla.jpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JpaApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
